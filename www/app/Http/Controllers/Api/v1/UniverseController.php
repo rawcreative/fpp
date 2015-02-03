e96e46e55e64ea776ec0270a288d137d38152745
T. Debo <tim@rawcreativestudios.com>
2015-02-03 17:57:39 -0500

    add universes endpoint

---
+++ www/app/Http/Controllers/Api/v1/UniverseController.php
@@ -0,0 +1,45 @@
+<?php namespace FPP\Http\Controllers\Api\v1;
+
+use FPP\Http\Requests;
+use FPP\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use League\Csv\Reader;
+
+class UniverseController extends Controller {
+
+	public function getUniverses()
+	{
+		if(!Cache::has('fpp_universes')) {
+			if (Storage::disk('pi')->exists("universes")) {
+				$csv       = Reader::createFromPath(config('fpp.universes'));
+				$entries   = $csv->fetchAssoc(['active', 'universe', 'startAddress', 'size', 'type', 'unicastAddress']);
+
+				Cache:put('fpp_universes', $entries, 60);
+			}
+		} else {
+			$entries = Cache::get('fpp_universes');
+		}
+
+		return response()->json([
+			'response' => [
+				'universes' => $entries
+			]
+		]);
+
+	}
+
+	public function getUniverse($universe)
+	{
+
+	}
+
+	public function deleteUniverse($universe)
+	{
+
+	}
+	
+
+}
--- www/app/Http/routes.php
+++ www/app/Http/routes.php
@@ -59,6 +59,8 @@ Route::group(['namespace' => 'Api\v1', 'prefix' => 'api'], function() {
 	get('/files/video', 'MediaController@getVideoFiles');
 	get('/files/sequence', 'MediaController@getSequenceFiles');
 
+	get('/universes', 'UniverseController@getUniverses');
+
 });
 
 Event::listen('status.request', function($data) {
