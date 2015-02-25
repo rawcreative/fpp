93730cba9606ef414c453976d355523665e8a9cc
T. Debo <tim@rawcreativestudios.com>
2015-02-25 00:46:33 -0500

    re-add plugin header file

---
+++ www/resources/views/plugins/header.blade.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <div class="page-meta">
+        <h1 class="page-title">Falcon Player Plugins</h1>
+        <div class="details">
+            <div class="date"></div>
+            <div class="status"></div>
+        </div>
+    </div>
+</header>
