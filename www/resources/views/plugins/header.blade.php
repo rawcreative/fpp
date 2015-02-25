a77a2a0ce289ddfef7f6e64e235bfdfc751a000a
T. Debo <tim@rawcreativestudios.com>
2015-02-25 00:13:19 -0500

    Revert "update gitignore"
    This reverts commit f8492cd69dfc1d881d31826d4f68351667618027.

--- www/resources/views/plugins/header.blade.php
+++
@@ -1,9 +0,0 @@
-<header class="header">
-    <div class="page-meta">
-        <h1 class="page-title">Falcon Player Plugins</h1>
-        <div class="details">
-            <div class="date"></div>
-            <div class="status"></div>
-        </div>
-    </div>
-</header>
